29e4d83fcd34dcfcfc1296809c29a3b7ff4855b2
Olga Sotula <olga.sotula@itctg.ru>
2022-10-21 18:00:46 +0300

    add preloader

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+import Preloader from '../Preloader/Preloader';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import './MoviesList.css';
 import { movies } from '../../utils/constants/Movies.js';
@@ -7,10 +8,13 @@ import FilterBar from '../FilterBar/FilterBar.js';
 
 const MoviesList = ({ isSaved }) => {
   const [isProcessing, setIsProcessing] = useState(false);
+
   return (
     <main className='movies'>
       <FilterBar/>
-      <MoviesCardList cards={movies} isSaved = { isSaved } />
+      {isProcessing ?
+        <Preloader/> : <MoviesCardList cards={movies} isSaved = { isSaved } />
+      }
     </main>
   );
 }
