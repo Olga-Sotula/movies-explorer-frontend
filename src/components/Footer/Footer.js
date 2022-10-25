926b33405c971bfa1c2a8d76be75d5f5b025774e
Olga Sotula <olga.sotula@itctg.ru>
2022-10-25 22:19:17 +0300

    add media 800 to movies

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
         <p className='footer__copyright'>&copy;2022</p>
         <ui className='footer__links'>
           <li><a className='footer__link' href='https://practicum.yandex.ru/' target='_blank'>Яндекс.Практикум</a></li>
-          <li><a className='footer__link' href='https://github.com/PavlovAles/' target='_blank'>Github</a></li>
+          <li><a className='footer__link' href='https://github.com/Olga-Sotula/' target='_blank'>Github</a></li>
         </ui>
       </div>
     </footer>
