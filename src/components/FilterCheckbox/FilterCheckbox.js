42e3f8dee7857bbb3e937aaaec7c23eda3c4dcc8
Olga Sotula <olga.sotula@itctg.ru>
2022-10-27 17:10:34 +0300

    fix: style Filter bar by figma

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -7,7 +7,6 @@ const FilterCheckBox = ({ modificator }) => {
     <div className={`filter-checkbox ${modificator}`}>
       <p className="filter-checkbox__text">Короткометражки</p>
       <button type="button" className="filter-checkbox__button filter-checkbox__button_active"/>
-
     </div>
   );
 }
