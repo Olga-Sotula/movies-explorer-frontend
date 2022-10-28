bb7630bc8d330477183e5eaed597c26c92e944e3
Olga Sotula <olga.sotula@itctg.ru>
2022-10-28 17:45:03 +0300

    fix: styled movies list by figma

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -7,14 +7,14 @@ import './MoviesCardList.css';
 const MoviesCardList = ({ cards, isSaved }) => {
   return (
     <section className='cards'>
-    <ul className='cards__list'>
-      {cards.map(card => (
-        <li key={card.id}>
-          <MoviesCard card={card} />
-        </li>
-      ))}
-    </ul>
-    <Button modificator="button_type_else">Ещё</Button>
+      <ul className='cards__list'>
+        {cards.map(card => (
+          <li key={card.id}>
+            <MoviesCard card={card} />
+          </li>
+        ))}
+      </ul>
+      <Button modificator="button_type_else">Ещё</Button>
     </section>
   )
 }
